d7ece28fad8be138af93c72a35c298ec0778056a
harish-ponna-au6 <harishponna@codalyze.com>
2021-02-03 12:01:00 +0530

    1

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ const App = _ => {
             padding: SIZES.padding3,
           }}
         >
+          
           <NavigationContainer>
             <NavigationScreens />
           </NavigationContainer>
